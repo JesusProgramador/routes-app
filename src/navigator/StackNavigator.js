bca1d29db95d7099f11845bdbf962a2fcdc8ef26
Jesus Fuentes <jesus.fuentes@lanek.cl>
2022-08-31 16:44:03 -0400

    create navigator

---
+++ src/navigator/StackNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import PermissionsScreen from '../screens/PermissionsScreen';
+import MapScreen from '../screens/MapScreen';
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white',
+        },
+      }}>
+      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      <Stack.Screen name="MapScreen" component={MapScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
