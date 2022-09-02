7fadcc653be3e7706a46f5e96eae33047457bfdf
Jesus Fuentes <jesus.fuentes@lanek.cl>
2022-09-01 21:45:18 -0400

    create map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,91 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useRef, useState} from 'react';
 import {StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Polyline} from 'react-native-maps';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['new NativeEventEmitter']);
+import useLocation from '../hooks/useLocation';
+import LoadingScreen from '../screens/LoadingScreen';
+import Fab from './Fab';
 
 const Map = () => {
+  const [showPolyline, setShowPolyline] = useState(true);
+  const {
+    hasLocation,
+    initialPosition,
+    getCurrentLocation,
+    followUserLocation,
+    stopFollowUserLocation,
+    userLocation,
+    routeLines,
+  } = useLocation();
+
+  const mapViewRef = useRef();
+  const following = useRef(true);
+
+  useEffect(() => {
+    followUserLocation();
+    return () => {
+      stopFollowUserLocation();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!following.current) {
+      return;
+    }
+
+    const {latitude, longitude} = useLocation;
+    mapViewRef.current?.animateCamera({
+      center: {latitude, longitude},
+    });
+  }, [userLocation]);
+
+  const centerPosition = async () => {
+    const {latitude, longitude} = await getCurrentLocation();
+    following.current = true;
+    mapViewRef.current?.animateCamera({
+      center: {latitude, longitude},
+    });
+  };
+
+  if (!hasLocation) {
+    return <LoadingScreen />;
+  }
+
   return (
     <>
       <MapView
+        ref={elem => (mapViewRef.current = elem)}
         style={styles.mapContainer}
         showsUserLocation
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialPosition.latitude,
+          longitude: initialPosition.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        onTouchStart={() => (following.current = false)}>
+        {showPolyline && (
+          <Polyline
+            coordinates={routeLines}
+            strokeColor="black"
+            strokeWidth={3}
+          />
+        )}
+      </MapView>
+
+      <Fab
+        iconName="compass-outline"
+        onPress={centerPosition}
+        style={{position: 'absolute', bottom: 15, right: 15}}
+      />
+
+      <Fab
+        iconName="brush-outline"
+        onPress={() => setShowPolyline(value => !value)}
+        style={{position: 'absolute', bottom: 80, right: 15}}
       />
     </>
   );
